977e136817dead4be50e5d58436aa07e5ff83c29
muttakin1991 <shawonmuttakin@gmail.com>
2020-07-19 09:49:50 -0700

    final work. but incomplete

---
+++ assingment.js
@@ -0,0 +1,35 @@
+function feetToMile(feet){
+    var mile = feet /5280;
+    return feet;
+}
+var result = feetToMile(50);
+console.log(result);
+
+
+
+function woodCalculator(chair, table, Khat){
+    var wood = 
+    var wood = chair * 1;
+    var wood = table * 3;
+    var wood = khat * 5;
+}
+
+
+
+
+function tinyFriend(firends){
+    var largest = firends[0];
+    for (var i = 0; i < firends.length; i++){
+        var currentFriends = firends[i];
+        if(currentFriends > largest){
+            largest = currentFriends;
+        }
+    }
+    return largest;
+}
+var largestFriends = tinyFriend([arif, asif, abdullah, alaminMia, ariful, Mohammod]);
+console.log(largestFriends);
+
+
+
+function(b)
